64fa3fb872c2b48d67552c36ab41cf381837e4d0
DrAyettey <danni.ayette@gmail.com>
2016-02-25 03:36:18 +0100

    the bakaren
    baker website

---
+++ potion.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Ayettey on 2016-02-24.
+ */
+
+function changePotion( newValue){
+
+    document.getElementById("slider").innerHTML=newValue;
+    document.getElementById("slider2").innerHTML=newValue;
+    document.getElementById("slider3").innerHTML=newValue;
+    document.getElementById("slider4").innerHTML=newValue;
+
+
+
+}
+
