0c77693a5e99c7df8207f84faea76eb8327b9883
DrAyettey <danni.ayette@gmail.com>
2016-03-04 02:45:13 +0100

    upadteVesion2
    update me

---
+++ changePotions.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Ayettey on 2016-03-03.
+ */
+
+
+function changePotion(newValue) {
+
+
+        document.getElementById("slider").innerHTML=newValue;
+        document.getElementById("slider2").innerHTML=newValue;
+        document.getElementById("slider3").innerHTML=newValue;
+        document.getElementById("slider4").innerHTML=newValue;
+        document.getElementById("slider5").innerHTML=newValue;
+        document.getElementById("slider6").innerHTML=newValue;
+        document.getElementById("slider8").innerHTML=newValue;
+        document.getElementById("slider7").innerHTML=newValue;
+        document.getElementById("slider9").innerHTML=newValue;
+
+
+
+
+}
+
+function ounce(){
+    var adder=24;
+    if(adder==24){
+        return adder+adder;
+    }
+
+}
+
